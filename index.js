aae98e976cb72e8f92e9269d62ce801b954e234d
enemy <nkalmykov937@gmail.com>
2021-08-06 13:01:03 +0300

    remove midleware

--- index.js
+++ index.js
@@ -74,19 +74,9 @@ app.use(
   cors({
     origin: "http://localhost:3000",
     methods: ["POST"],
-    credentials: false,
+    credentials: true,
   })
 );
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "DELETE, PUT, GET, POST");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
 
 app.use(passport.initialize());
 app.use(passport.session());
